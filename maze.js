d53e1f4d2a87bb8f4210d3afb361b10d665725c9
Rhon-Kaniel Bramwell <xphat@xphat.net>
2018-10-18 00:18:51 -0500

    Exercise 2 - All Boundaries Glow Red On Hover Completed!

--- maze.js
+++ maze.js
@@ -1,6 +1,9 @@
 window.onload = function(){
-   var b = document.getElementsByClassName("boundary");
-   b[0].addEventListener("mouseover", function() { b[0].classList.add("youlose")});
+   var b = document.querySelectorAll(".boundary");
+   for (var i = 0; i < b.length; i++)
+   {
+	   b[i].addEventListener("mouseover", function() { this.classList.add("youlose");}, false);
+   }
 
    };
 
