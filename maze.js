d2e6f8e531c56fb5d030d9ef4b461c76db6d0abe
Rhon-Kaniel Bramwell <xphat@xphat.net>
2018-10-17 23:51:27 -0500

    Exercise 1 - Single boundary Turns Red Completed!

---
+++ maze.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+   var b = document.getElementsByClassName("boundary");
+   b[0].addEventListener("mouseover", function() { b[0].classList.add("youlose")});
+
+   };
+
+
+
+
