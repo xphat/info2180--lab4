0a71a7fb4dab84f950ea3e77eca5b011a70691f1
Rhon-Kaniel Bramwell <xphat@xphat.net>
2018-10-18 00:33:36 -0500

    Exercise 3 - Alerts on Successful Completion of Maze Completed!

--- maze.js
+++ maze.js
@@ -1,9 +1,20 @@
 window.onload = function(){
    var b = document.querySelectorAll(".boundary");
+   var haslost = false;
    for (var i = 0; i < b.length; i++)
    {
-	   b[i].addEventListener("mouseover", function() { this.classList.add("youlose");}, false);
+	   b[i].addEventListener("mouseover", function() { this.classList.add("youlose"); haslost = true;}, false);
    }
+   var e = document.getElementById("end");
+   
+   
+   function checkiflost()
+   {
+	   if (haslost == false)
+		   alert("You Win!");
+   }
+   
+   e.addEventListener("mouseover", checkiflost, false);
 
    };
 
