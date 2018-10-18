92758cd3c862d24a5c9d4cf35ab6dec2aee68e0e
Rhon-Kaniel Bramwell <xphat@xphat.net>
2018-10-18 00:49:14 -0500

    Exercise 5 - On-Page Status Updates Completed! (Also Moved Around Some Variable Declarations)

--- maze.js
+++ maze.js
@@ -1,21 +1,30 @@
 window.onload = function(){
    var b = document.querySelectorAll(".boundary");
    var haslost = false;
+   var stts = document.getElementById("status");
+   var e = document.getElementById("end");
+   var s = document.getElementById("start");
+   
    for (var i = 0; i < b.length; i++)
    {
 	   b[i].addEventListener("mouseover", function() { this.classList.add("youlose"); haslost = true;}, false);
    }
-   var e = document.getElementById("end");
 
    function checkiflost()
    {
+	   
 	   if (haslost == false)
-		   alert("You Win!");
+	   {
+		   stts.innerHTML = "You Win!"
+	   }
+	   else
+	   {
+		   stts.innerHTML = "You Lose!"
+	   }
+		   
    }
 
-   e.addEventListener("mouseover", checkiflost, false);
-   
-   var s = document.getElementById("start");
+   e.addEventListener("mouseover", checkiflost, false); 
    s.addEventListener("click", resetGame, false);
    
    function resetGame()
@@ -24,6 +33,7 @@ window.onload = function(){
 		{
 		   b[i].classList.remove("youlose");
 		   haslost = false;
+		   stts.innerHTML = "Move your mouse over the \"S\" to begin."
 		}
    }
 
