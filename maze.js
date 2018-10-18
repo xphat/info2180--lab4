945000bd08c68e1dd42cecb9087142f76d7238b0
Rhon-Kaniel Bramwell <xphat@xphat.net>
2018-10-18 00:40:01 -0500

    Exercise 4 - Restartable Maze Completed!

--- maze.js
+++ maze.js
@@ -6,15 +6,26 @@ window.onload = function(){
 	   b[i].addEventListener("mouseover", function() { this.classList.add("youlose"); haslost = true;}, false);
    }
    var e = document.getElementById("end");
-   
-   
+
    function checkiflost()
    {
 	   if (haslost == false)
 		   alert("You Win!");
    }
-   
+
    e.addEventListener("mouseover", checkiflost, false);
+   
+   var s = document.getElementById("start");
+   s.addEventListener("click", resetGame, false);
+   
+   function resetGame()
+   {
+		for (var i = 0; i < b.length; i++)
+		{
+		   b[i].classList.remove("youlose");
+		   haslost = false;
+		}
+   }
 
    };
 
